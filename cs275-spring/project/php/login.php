e610382f6899ba807794855fdbc1f2cd4f5f30eb
jared <j-wasinger@hotmail.com>
2014-05-25 22:10:56 -0700

    .

--- cs275-spring/project/php/login.php
+++ cs275-spring/project/php/login.php
@@ -2,4 +2,9 @@
 	session_start()
 	$username = mysql_real_escape_string(_POST['password']);
 	$password = md5($password);
+
+	//see if the user entered the correct credentials
+	$login = mysql_query("SELECT * FROM users 								\
+						  WHERE (username='".$username."') AND (password=)
+		")
 ?>
