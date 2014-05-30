b28cfc6235c3c95c63ca1161c4ee3b16c89533a2
jared <j-wasinger@hotmail.com>
2014-05-29 22:57:20 -0700

    .

--- cs275-spring/project/php/login.php
+++ cs275-spring/project/php/login.php
@@ -4,19 +4,20 @@ include "connect.php";
 
 $GLOBALS['user_login_fail'] = false;
 
-$username = mysql_real_escape_string($_POST['wasingej']);
+$username = mysql_real_escape_string($_POST['username']);
 $password = $_POST['password'];
 
 //for now, store passwords in plain text
 //$password = md5($password);
 
 //see if the user entered the correct credentials
-$login = mysql_query('SELECT * FROM users 								
+$login = mysql_query('SELECT * FROM Users 								
 					  WHERE (username="'.$username.'") AND (password="'.$password.'")');
 
 if($login && mysql_num_rows($login) == 1)
 {
 	session_start();
+	
 	$_SESSION['username'] = $_POST['username'];
 	header('Location: user_home.php');
 }
--- cs275-spring/project_writeup/relations.sql
+++ cs275-spring/project_writeup/relations.sql
@@ -12,10 +12,15 @@ CREATE TABLE Manager(	username	CHAR(32),
 						PRIMARY KEY (username),
 					);
 
-CREATE TABLE Order(		order_id 	INTEGER,
+CREATE TABLE Order(		customer    CHAR(32)
+						order_id 	INTEGER,
 						cost	 	REAL,
 
 						PRIMARY KEY (order_id),
+
+						FOREIGN KEY (customer) REFERENCES Customer 
+							ON DELETE CASCADE
+							ON UPDATE CASCADE
 				  );
 
 CREATE TABLE Item(		name	CHAR(32),
@@ -23,6 +28,18 @@ CREATE TABLE Item(		name	CHAR(32),
 
 						PRIMARY KEY (name),
 				);
+
+CREATE TABLE Order_Item(	item    CHAR(32),
+							order   INTEGER,
+
+							FOREIGN KEY item REFERENCES Item,
+								ON UPDATE CASCADE
+								ON DELET RESTRICT
+
+							FOREIGN KEY order REFERENCES Order,
+								ON UPDATE CASCADE
+								ON DELET RESTRICT
+					   );			
 
 CREATE TABLE Ingredient (	order_id		INTEGER,
 							name 			CHAR(32),
