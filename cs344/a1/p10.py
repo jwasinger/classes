3ff141d6e86fdbf719d314186119b70b70f1a641
j-wasinger@hotmail.com <j-wasinger@hotmail.com>
2014-10-09 16:33:30 -0700

    .

--- cs344/a1/p10.py
+++ cs344/a1/p10.py
@@ -1,3 +1,5 @@
+#! /usr/bin/python
+
 from scipy import stats
 import numpy
 import math
@@ -6,53 +8,69 @@ import sys
 
 def mean(vals):
     num_vals = len(vals)
-    sum_vals = 0
+    sum_vals = 0.0
     
     for i in range(num_vals):
-        sum_vals += vals[i]
+        sum_vals += float(vals[i])
     
     return float(sum_vals)/float(num_vals)
+
 
 def stdev(vals):
     mean_vals = mean(vals)
 
-    sum_squared = 0
+    sum_squared = 0.0
     for i in range(len(vals)):
-        sum_squared += ((mean_vals - float(vals[i])) ** 2)
+        sum_squared += ((mean_vals - float(vals[i])) ** 2.0)
     
-    return math.sqrt(float(sum_squared)/float(len(vals) - 1))
+    return math.sqrt(float(sum_squared)/float(len(vals) - 1.0))
+
 
 def skewness(vals):
     std_vals = float(stdev(vals)) 
     mean_vals = float(mean(vals))
-
-    numerator = 0
+   
+    numerator = 0.0
     for i in range(len(vals)):
-        numerator += (((float(vals[i]) - mean_vals)/std_vals) ** 3)
+        numerator += ((vals[i] - mean_vals) ** 3.0)
     
-    return numerator / len(vals)
+    denominator = 0.0
+    for i in range(len(vals)):
+        denominator += ((vals[i] - mean_vals) ** 2.0)
+    
+    denominator = denominator ** (3.0/2.0)
+
+    return (numerator / denominator) * math.sqrt(len(vals))
 
 
 def kurtosis(vals):
     std_vals = stdev(vals)
     mean_vals = mean(vals)
     
-    numerator = 0
+    numerator = 0.0
     for i in range(len(vals)):
-        numerator += (vals[i] - mean_vals) ** 4
+        numerator += (vals[i] - mean_vals) ** 4.0
 
-    denominator = 0
+    denominator = 0.0
     for i in range(len(vals)):
-        denominator += (vals[i] - mean_vals) ** 2
+        denominator += (vals[i] - mean_vals) ** 2.0
     
-    denominator = denominator ** 2
+    denominator = denominator ** 2.0
 
-    return len(vals) * (numerator/denominator)
+    return float(len(vals)) * (numerator/denominator)
 
 if __name__ == "__main__":
     
     vals = sys.stdin.read().split('\n')
-
+    for i in range(len(vals)):
+        if not vals[i]:
+            del vals[i]
+        else:
+            vals[i] = float(vals[i])
+    
+    print vals
+    print len(vals)
+        
     my_mean = mean(vals)
     true_mean = numpy.mean(vals)
 
@@ -60,10 +78,11 @@ if __name__ == "__main__":
     true_std = numpy.std(vals, ddof=1)
 
     my_skew = skewness(vals)
-    true_skew = stats.skew(vals, None, False)
-    
-    pdb.set_trace()
+    true_skew = stats.skew(vals, None, True)
 
     my_kurtosis = kurtosis(vals)
-    true_kurtosis = stats.kurtosis(vals, None, False, False)
-     
+    true_kurtosis = stats.kurtosis(vals, None, False, True)
+    
+    print "My Calc:  %s %s %s %s %s" % (len(vals), my_mean, my_std, my_skew, my_kurtosis)
+    print "Lib Calc: %s %s %s %s %s" % (len(vals), true_mean, true_std, true_skew, true_kurtosis)
+    
--- cs344/a1/p7.py
+++ cs344/a1/p7.py
@@ -1,3 +1,5 @@
+#! /bin/python
+
 import sys, urllib2, pdb
 
 if __name__ == "__main__":
