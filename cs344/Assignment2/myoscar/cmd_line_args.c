ada542b20f4a1240c0253c4d625e8c969edccf3c
jared <j-wasinger@hotmail.com>
2014-10-19 19:22:05 -0700

    .

--- cs344/Assignment2/myoscar/cmd_line_args.c
+++ cs344/Assignment2/myoscar/cmd_line_args.c
@@ -11,7 +11,9 @@ int proc_cmd_line_args(int argc, char **argv, CMDArgs *out_cmd_args)
     int files_write_pos = 0;
     int actions = 0;
     int num_actions = 0;
-    int i = 0;
+    int i = 1;
+    short has_arc_file = 0;
+
     for(; i < argc; i++)
     {
         if(strcmp(argv[i], "-a") == 0)
@@ -19,90 +21,111 @@ int proc_cmd_line_args(int argc, char **argv, CMDArgs *out_cmd_args)
             //note: this option always takes precendence over -A
             actions |= ACTION_ADD_MEMBERS;
             num_actions++;
+            continue;
         }
-        if(strcmp(argv[i], "-A") == 0)
+        else if(strcmp(argv[i], "-A") == 0)
         {
             actions |= ACTION_ADD_ALL;
             num_actions++;
+            continue;
         }
-        if(strcmp(argv[i], "-C") == 0)
+        else if(strcmp(argv[i], "-C") == 0)
         {
             actions |= ACTION_CLEANSE;
             num_actions++;
+            continue;
         }
-        if(strcmp(argv[i], "-d") == 0)
+        else if(strcmp(argv[i], "-d") == 0)
         {
             actions |= ACTION_DELETE;
             num_actions++;
+            continue;
         }
-        if(strcmp(argv[i], "-e") == 0)
+        else if(strcmp(argv[i], "-e") == 0)
         {
             actions |= ACTION_EXTRACT;
             num_actions++;
+            continue;
         }
-        if(strcmp(argv[i], "-E") == 0)
+        else if(strcmp(argv[i], "-E") == 0)
         {
             actions |= ACTION_EXTRACT_CURR_TIME;
             num_actions++;
+            continue;
         }
-        if(strcmp(argv[i], "-h") == 0)
+        else if(strcmp(argv[i], "-h") == 0)
         {
             actions |= ACTION_HELP;
             num_actions++;
+            continue;
         }
-        if(strcmp(argv[i], "-m") == 0)
+        else if(strcmp(argv[i], "-m") == 0)
         {
             actions |= ACTION_MARK;
             num_actions++;
+            continue;
         }
-        if(strcmp(argv[i], "-o") == 0)
+        else if(strcmp(argv[i], "-o") == 0)
         {
             actions |= ACTION_OVERWRITE;
             num_actions++;
+            continue;
         }
-        if(strcmp(argv[i], "-t") == 0)
+        else if(strcmp(argv[i], "-t") == 0)
         {
             actions |= ACTION_TOC;
             num_actions++;
+            continue;
         }
-        if(strcmp(argv[i], "-T") == 0)
+        else if(strcmp(argv[i], "-T") == 0)
         {
             actions |= ACTION_LONG_TOC;
             num_actions++;
+            continue;
         }
-        if(strcmp(argv[i], "-u") == 0)
+        else if(strcmp(argv[i], "-u") == 0)
         {
             actions |= ACTION_UNMARK;
             num_actions++;
+            continue;
         }
-        if(strcmp(argv[i], "-v") == 0)
+        else if(strcmp(argv[i], "-v") == 0)
         {
             actions |= ACTION_VERBOSE;
             num_actions++;
+            continue;
         }
-        if(strcmp(argv[i], "-V") == 0)
+        else if(strcmp(argv[i], "-V") == 0)
         {
             actions |= ACTION_VERSION;
             num_actions++;
-        }
-        else if (!out_cmd_args->arc_file)
-        {
-            strcpy(out_cmd_args->arc_file, argv[i]);
             continue;
         }
-        else
+        else if(argv[i][0] == '-')
         {
-            //the command line arg can only be an archive file member
-            if(!out_cmd_args->files)
-            {
-                num_files = argc - (num_actions+1);
+            printf("unrecognized command");
+            return -1;
+            continue;
+        }
 
-                out_cmd_args->files = malloc(sizeof(char*) * num_files);
-                out_cmd_args->files[files_write_pos] = malloc(sizeof(char) * strlen(argv[i]));
-            }
+        if (!has_arc_file)
+        {
+            strcpy(out_cmd_args->arc_file, argv[i]);
+            has_arc_file = 1;
+            continue;
+        }
+    
+        //the command line arg can only be an archive file member
+        if(!out_cmd_args->files)
+        {
+            num_files = argc - (num_actions+2);
+
+            out_cmd_args->files = malloc(sizeof(char*) * num_files);
+            out_cmd_args->files[files_write_pos] = malloc(sizeof(char) * strlen(argv[i]));
         }
     }
-  
+    
+    out_cmd_args->actions = actions;
     out_cmd_args->num_files = num_files;
     return 0;
 }
