0162b8f4e6645e41f187df44540790d00732042c
jared <j-wasinger@hotmail.com>
2014-11-15 20:45:07 -0800

    .:

--- cs344/Assignment3/fifo_client.c
+++ cs344/Assignment3/fifo_client.c
@@ -4,12 +4,15 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <string.h>
+#include <linux/limits.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 
 #define BUF_SIZE 256
 
 char client_reader_name[PATH_MAX];
 char client_writer_name[PATH_MAX];
-char server_fifo_name[PATH];
+char server_fifo_name[PATH_MAX];
 
 int server_fifo_fd = -1;
 int client_writer_fd = -1;
@@ -18,6 +21,28 @@ int client_reader_fd = -1;
 void delete_fifos(void);
 void pid_to_char(int pid, char **out);
 int str_starts_with(char *str, char *str2); // http://stackoverflow.com/questions/15515088/how-to-check-if-string-starts-with-certain-string-in-c
+void print_error(void);
+void strip_char(char *str, char c);
+
+
+void strip_char(char *str, char c)
+{
+    int size = strlen(str);
+    int i = 0;
+
+    for(; i < size; i++)
+    {
+        if(str[i] == c)
+            str[i] = '\0'; // a terrible unforgivable hack ;)
+    }
+}
+
+void print_error(void)
+{
+    int error = errno;
+    printf("Error: %s\n", strerror(error));    
+}
+
 
 int str_starts_with(char *str, char *str2)
 {
@@ -32,98 +57,173 @@ void delete_fifos(void)
     unlink(client_writer_name);    
 }
 
-void pid_to_char(int pid, char **out)
-{
-    short first, second;    
-}
-
 int main(int argc, char **argv)
 {
     char read_buf[BUF_SIZE];
+    char tmp_buf[BUF_SIZE];
+    char path_buf[PATH_MAX];
     int size_read = -1;
+    int res = 0;
+    char pid_str[10];
+    memset(pid_str, 0, 10);
 
     umask(0);
 
-    CREATE_SERVER_FIFO_NAME(server_fifo_name, (int)getpid());
+    CREATE_SERVER_FIFO_NAME(server_fifo_name);
     CREATE_CLIENT_READER_NAME(client_reader_name, (int)getpid());
     CREATE_CLIENT_WRITER_NAME(client_writer_name, (int)getpid());
     
-    mkfifo(client_reader_name, FIFO_PERMISSIONS);
-    mkfifo(client_writer_name, FIFO_PERMISSIONS);
-    
-    client_reader_fd = open(client_reader_name, O_RONLY);
-    client_writer_fd = open(client_writer_name, O_WRONLY);
-
+    res = mkfifo(client_reader_name, FIFO_PERMISSIONS);
+    res = mkfifo(client_writer_name, FIFO_PERMISSIONS);
     atexit(delete_fifos);
-    
-    char pid_str[10];
-    res = sprintf(pid_str, "%d");
+    FILE *fp = NULL;
+
+    res = sprintf(pid_str, "%d", getpid());
     if(res == -1)
     {
         //print error    
         return -1;
     }
-     
+
+    server_fifo_fd = open(server_fifo_name, O_WRONLY);
+    if(server_fifo_fd == -1)
+    {
+        //print error
+        print_error();
+        return -1;
+    }     
+
     res = write(server_fifo_fd, pid_str, res);     
     if(res == -1)
     {
         //print error
+        print_error();
         return -1;    
     }
-    
+
+    client_reader_fd = open(client_reader_name, O_RDONLY);
+    client_writer_fd = open(client_writer_name, O_WRONLY);
+
     while(1)
     {
         //read from the command line
         memset(read_buf, 0, BUF_SIZE);
-        res = read(STDIN_FILENO, read_buf, BUF_SIZE);
+        /*res = read(STDIN_FILENO, read_buf, BUF_SIZE);
         if(res == -1)
         {
             //print error
             return -1;    
-        }
-        
-        size_read = res;
+        }*/
+        fgets(read_buf, BUF_SIZE, stdin);
 
         //write the command to the server
-        res = write(client_writer_fd, read_buf, size_read);    
+        res = write(client_writer_fd, read_buf, BUF_SIZE);    
         if(res == -1)
         {
             //print error
+            print_error();
             return -1;    
         }
         
-        if(strncmp(read_buf, "ldir", size_read) == 0)
+        if(strncmp(read_buf, "exit", 4) == 0)
         {
-            
+            return 0;
         }
-        else if(strncmp(read_buf, "lcd", size_read) == 0)
+        else if(strncmp(read_buf, "ldir", 4) == 0)
         {
-            
+            fp = popen(CMD_LS_POPEN, "r");
+            if(fp == NULL)
+            {
+                printf("error");
+                return -1;
+            }
+
+            printf("client process directory listing: \n");
+            while(fgets(path_buf, PATH_MAX, fp))
+                printf("%s", path_buf);
+
+            continue;
         }
-        else if(strncmp(read_buf, "lpwd", size_read) == 0)
+        else if(strncmp(read_buf, "lcd ", 4) == 0)
         {
-                
+            char *new_dir = read_buf + 4;
+            strip_char(new_dir, '\n');
+
+            res = chdir(new_dir);
+            if(res == -1)
+            {
+                if(errno == ENOENT)
+                {
+                    printf("no such file or directory");
+                    continue;
+                }
+                print_error();
+                return -1;
+            }
+
+            if(getcwd(tmp_buf, BUF_SIZE) == NULL)
+            {
+                print_error();
+                return -1;
+            }
+
+            printf("new directory for client process: %s\n", tmp_buf);
+            continue;
         }
-        else if(strncmp(read_buf, "dir", size_read) == 0)
+        else if(strncmp(read_buf, "lpwd", 4) == 0)
         {
-            //read the server's response
-             
+            getcwd(tmp_buf, PATH_MAX);
+            printf("client pwd: %s\n", tmp_buf);
+            continue;
+        }
+        else if(strncmp(read_buf, "dir", 3) == 0)
+        {
+            res = read(client_reader_fd, tmp_buf, BUF_SIZE);
+            if(res == -1)
+            {
+                print_error();
+                return -1;
+            }
+
+            printf(tmp_buf);
         }
         else if(strncmp(read_buf, "cd ", 3) == 0)
         {
-            
+            //read the server's response
+            res = read(client_reader_fd, tmp_buf, BUF_SIZE);
+            if(res == -1)
+            {
+                print_error();
+                return -1;
+            }
+            else if(res == 0)
+            {
+                printf("no response from the server\n");
+                return -1;
+            }
+
+            printf("server pwd: %s\n", tmp_buf);
         }
         else if(strncmp(read_buf, "exit", size_read) == 0)
         {
-                
+            exit(0);
         }
-        else if(strncmp(read_buf, "pwd", size_read) == 0)
+        else if(strncmp(read_buf, "pwd", 3) == 0)
         {
-            
+            res = read(client_reader_fd, tmp_buf, BUF_SIZE);
+            if(res == -1)
+            {
+                print_error();
+                return -1;
+            }
+
+            printf("server pwd is: %s\n", tmp_buf);
+            continue;
         }
         else if(strncmp(read_buf, "help", size_read) == 0)
         {
-                
+            printf("commands are: dir, pwd, ldir, lpwd, cd, lcd\n");
+            continue;
         }
         //read the server's response
     }
--- cs344/Assignment3/fifo_server.c
+++ cs344/Assignment3/fifo_server.c
@@ -4,20 +4,54 @@
 #include <stdlib.h>
 #include <signal.h>
 #include <string.h>
+#include <errno.h>
+#include <linux/limits.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 
 #include "fifo.h"
 
-#define BUF_SIZE 1024 
+#define BUF_SIZE 256 
 
-int get_pid(char *str); // grab the first two characters from str and convert them into an int (the process id)
+char server_fifo_name[PATH_MAX];
+char client_reader_name[PATH_MAX];
+char client_writer_name[PATH_MAX];
+char path_buf[PATH_MAX];
+
+void strip_char(char *str, char c);
+int pid_str_to_int(char *str); // grab the first two characters from str and convert them into an int (the process id)
+void print_error(void);
+void child_proc_exit(int sig);
+
+void strip_char(char *str, char c)
+{
+    int size = strlen(str);
+    int i = 0;
+
+    for(; i < size; i++)
+    {
+        if(str[i] == c)
+            str[i] = '\0'; // a terrible unforgivable hack ;)
+    }
+}
 
 //http://stackoverflow.com/questions/1294649/cleanest-way-to-combine-two-shorts-to-an-int
-int get_pid(char *str)
+int pid_str_to_int(char *str)
 {
-    short first_part = (short)str[0];
-    short second_part = (short)str[1];    
+    return atoi(str);
+}
 
-    return ((first_part<<16) | ((second_part) & 0xffff));
+void child_proc_exit(int sig)
+{
+    wait(NULL);
+}
+
+static void my_exit(void)
+{
+    unlink(server_fifo_name);
+    unlink(client_reader_name);
+    unlink(client_writer_name);
 }
 
 void print_error(void)
@@ -29,28 +63,26 @@ void print_error(void)
 int main(int argc, char **argv)
 {
     int res = 0;
-    char server_fifo_name[PATH_MAX];
-    char reader_name[PATH_MAX];
-    char writer_name[PATH_MAX];
-    char client_reader_name[PATH_MAX];
-    char client_writer_name[PATH_MAX];
     
     char msg_from_client[256];
-    char tmp_buff[BUF_SIZE];
+    char tmp_buf[BUF_SIZE];
 
     int server_read_fd = -1;
-    int server_write_fd = -1;
+    //int server_write_fd = -1;
     int client_read_fd = -1;
     int client_write_fd = -1;
-    //umask(0)
+    FILE *fp = NULL;
+
+    umask(0);
 
     //create the server fifo
     CREATE_SERVER_FIFO_NAME(server_fifo_name);
     res = mkfifo(server_fifo_name, FIFO_PERMISSIONS);
+    atexit(my_exit);
 
     server_read_fd = open(server_fifo_name, O_RDONLY);
 
-    server_write_fd = open(server_fifo_name, O_WRONLY);
+    //server_write_fd = open(server_fifo_name, O_WRONLY);
 
     //signal(SIGPIPE, SIG_IGN);
     //signal(SIGINT, exit_handler)
@@ -64,69 +96,121 @@ int main(int argc, char **argv)
             //exit here    
             return -1;
         }
-        
-        //set the child exit handler to make the parent wait until child is fully closed
-        
-        switch(fork())
+        if(res != 0)
         {
-            case -1:
-                //report an error
-                break;
-            case 0:
-                CREATE_CLIENT_READER_NAME(client_reader_name, get_pid(msg_from_client));
-                CREATE_CLIENT_WRITER_NAME(client_writer_name, get_pid(msg_from_client));
-                client_read_fd = open(client_writer_name, O_WRONLY);
-                client_write_fd = open(client_reader_name, O_RDONLY);
-                 
-                while(strcmp(msg_from_client, "exit") != 0)
-                {
-                    if(strncmp(msg_from_client, "cd", 2) == 0)
+            printf("client connected\n");
+            //set the child exit handler to make the parent wait until child is fully closed
+            
+            signal(SIGCHLD, child_proc_exit);
+            switch(fork())
+            {
+                case -1:
+                    //report an error
+                    break;
+                case 0:
+
+                    CREATE_CLIENT_READER_NAME(client_reader_name, pid_str_to_int(msg_from_client));
+                    CREATE_CLIENT_WRITER_NAME(client_writer_name, pid_str_to_int(msg_from_client));
+                    
+                    client_read_fd = open(client_reader_name, O_WRONLY);
+                    client_write_fd = open(client_writer_name, O_RDONLY);
+                    
+                    while(1)
                     {
-                        char *new_dir = msg_from_client + 3;
-                        res = chdir(new_dir);
+                        res = read(client_write_fd, msg_from_client, BUF_SIZE);
                         if(res == -1)
                         {
-                            print_error();
-                            return -1;                                
-                        }
-                        
-                        if(getcwd(tmp_buf, BUF_SIZE) == NULL)
-                        {
-                            print_error();
+                            printf("Error!\n");
                             return -1;
                         }
-
-                        res = write(client_read_fd, tmp_buf, BUF_SIZE);
-                        if(res == -1)
+                        if(res == 0)
                         {
-                            print_error();    
+                            continue;
                         }
 
-                    }
-                    else if(strcmp(msg_from_client, "dir") == 0)
-                    {
+                        if(strncmp(msg_from_client, "exit", 4) == 0)
+                            break; 
                     
-                    }
-                    else if(strcmp(msg_from_client,  "pwd") == 0)
-                    {
-                        memcpy(tmp_buff, 0, BUF_SIZE);
-                        getcwd(tmp_buff, BUF_SIZE);
-                        write(client_read_fd, tmp_buff, BUF_SIZE);                     
-                    }
-                    else if(strcmp(msg_from_client, "help") == 0)
-                    {
-                            
-                    }
-                    else
-                    {
-                        //error message!!!    
-                    }
-                }
+                        
+                        if(strncmp(msg_from_client, "cd", 2) == 0)
+                        {
+                            char *new_dir = msg_from_client + 3;
+                            strip_char(new_dir, '\n');
 
-                //clean up 
-                break;
-            default:
-                break;    
+                            res = chdir(new_dir);
+                            if(res == -1)
+                            {
+                                if(errno == ENOENT)
+                                {
+                                    res = write(client_read_fd, "No such file or directory\n\0", 27);
+                                    continue;
+                                }
+                                print_error();
+                                return -1;                                
+                            }
+                            
+                            if(getcwd(tmp_buf, BUF_SIZE) == NULL)
+                            {
+                                print_error();
+                                return -1;
+                            }
+
+                            res = write(client_read_fd, tmp_buf, BUF_SIZE);
+                            if(res == -1)
+                            {
+                                print_error();
+                                return -1;    
+                            }
+
+                        }
+                        else if(strcmp(msg_from_client, "dir") == 0)
+                        {
+                            fp = popen(CMD_LS_POPEN, "r");
+                            if(fp == NULL)
+                            {
+                                printf("error");
+                                return -1;
+                            }
+
+                            while(fgets(path_buf, PATH_MAX, fp))
+                                sprintf(tmp_buf, "%s", path_buf);
+
+                            res = write(client_read_fd, tmp_buf, PATH_MAX);
+                            if(res == -1)
+                            {
+                                print_error();
+                                return -1;
+                            }
+                        }
+                        else if(strncmp(msg_from_client, "pwd", 3) == 0)
+                        {
+                            memset(tmp_buf, 0, BUF_SIZE);
+                            getcwd(tmp_buf, BUF_SIZE);
+                            write(client_read_fd, tmp_buf, BUF_SIZE);                     
+                        }
+                        else if(strcmp(msg_from_client, "help") == 0)
+                        {
+                            continue;
+                        }
+                        else
+                        {
+                            //error message!!!    
+                        }
+
+                        memset(msg_from_client, 0, BUF_SIZE);
+                        memset(tmp_buf, 0, BUF_SIZE);
+                    }
+
+                    close(client_write_fd);
+                    close(client_read_fd);
+                    //clean up 
+
+                    printf("client disconnected\n");
+                    return 0;
+                    break;
+                default:
+                    break;    
+            }
         }
     }
 }
