735230b1bf72a9c9256c97b38b7b4a880624d70b
Jared Wasinger <j-wasinger@hotmail.com>
2015-02-17 20:00:47 -0800

    .

---
+++ cs551/project2/src/matlib.cpp
@@ -0,0 +1,89 @@
+/*
+
+Dummy routines for matrix transformations.
+
+These are for you to write!
+
+*/
+
+
+#include <stdio.h>
+#include <math.h>
+#include <fstream>
+#include <Windows.h>
+#include <ostream>
+#include <sstream>
+#include <string>
+
+#include "osuGraphics.h"
+#include "lines.h"
+
+//-------------------------------------------------
+
+
+void osuOrtho(double left, double right, double bottom, double top, double nearp,double farp)
+{ 
+
+}
+void osuPerspective(double fovy, double nearp, double farp) 
+{  	
+}
+void osuBegin(OSUDrawable mode)
+{
+}
+
+void osuEnd()
+{
+}
+
+void osuColor3f(double red, double green, double blue)
+{
+}
+
+void osuVertex2f(double x, double y)
+{
+}
+
+
+
+void osuVertex3f(double x, double y, double z)
+{
+	
+}
+
+void osuInitialize() 
+{ 
+}
+
+
+void osuPushMatrix() 
+{ 
+
+}
+
+void osuPopMatrix() 
+{ 
+}
+
+void osuLoadIdentityMatrix()
+{
+}
+void osuTranslate(double tx, double ty, double tz) 
+{ 
+}
+
+void osuScale(double sx, double sy, double sz) 
+{ 
+}
+
+
+
+void osuRotate(double angle, double ax, double ay, double az) { 
+
+	
+}
+
+void osuLookat(double from[3], double at[3], double up[3])
+{
+}
+
